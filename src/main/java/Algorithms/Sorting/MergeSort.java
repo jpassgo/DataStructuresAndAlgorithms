c7ff013803ef4842e680595f42f6ef245fc1cbcc
Jeff Pascoe <jeffpascoe@jeffs-mbp.attlocal.net>
2020-04-07 19:53:10 -0500

    Adding mergeSort implementation. Tests to come shortly.

--- src/main/java/Algorithms/Sorting/MergeSort.java
+++ src/main/java/Algorithms/Sorting/MergeSort.java
@@ -4,12 +4,20 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
+        int[] array = new int[] {232, 5, 23, 2, 1, 443, 52};
 
+        for(int item : array) System.out.print(String.format("%s ", item));
+
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.mergeSort(array, 0, array.length-1);
+
+        System.out.println("");
+        for(int item : array) System.out.print(String.format("%s ", item));
     }
 
     private void mergeSort(int[] array, int start, int end) {
         if(start < end) {
-            int middle = (end+start)/2;
+            int middle = start + (end - start) / 2;
             mergeSort(array, start, middle);
             mergeSort(array, middle+1, end);
             merge(array, start, middle, end);
@@ -17,20 +25,24 @@ public class MergeSort {
     }
 
     private void merge(int[] array, int start, int middle, int end) {
-        int[] left = Arrays.copyOfRange(array, start, middle);
-        int[] right = Arrays.copyOfRange(array, middle + 1, end);
+        int length = end - start + 1;
+        int[] temp = new int[length];
 
-        int leftLength = left.length;
-        int rightLength = right.length;
-
-        int i = 0, j = 0, k = start;
-        while(i < leftLength && j < rightLength) {
-            if(left[i] < right[j]) {
-                array[k++] = left[i++];
+        int i = start, j = middle + 1, k = 0;
+        while(i <= middle && j <= end) {
+            if(array[i] <= array[j]) {
+                temp[k++] = array[i++];
             } else {
-                array[k++] = right[j++];
+                temp[k++] = array[j++];
             }
         }
 
+        while(i <= middle) {
+            temp[k++] = array[i++];
+        }
+        while(j <= end) {
+            temp[k++] = array[j++];
+        }
+        System.arraycopy(temp, 0, array, start, length);
     }
 }
