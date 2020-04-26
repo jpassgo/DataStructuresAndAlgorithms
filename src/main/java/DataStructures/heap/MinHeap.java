575ecc556442339d086138ff29324491dd9bfffb
jpassgo <jeffpascoe9@gmail.com>
2020-04-25 23:50:07 -0500

    min heap implementation.

--- src/main/java/DataStructures/heap/MinHeap.java
+++ src/main/java/DataStructures/heap/MinHeap.java
@@ -4,22 +4,75 @@ public class MinHeap<T extends Comparable<? super T>> {
 
     private T[] heap;
     private int size;
+    private static final int CAPACITY = 30;
 
-    void insert(T t) {
-
+    public MinHeap(int size) {
+        this.size = size;
+        this.heap = (T[])new Comparable[CAPACITY];
     }
 
-    private int parent(int index) {
+    void insert(T t) {
+        heap[++size] = t;
+        bubbleUp();
+    }
+
+    void bubbleUp() {
+        int index = this.size;
+        while(isParentPresent(index) && parent(index).compareTo(heap[index]) > 0) {
+            swap(parentIndex(index), index);
+            index = parentIndex(index);
+        }
+    }
+
+    private boolean isParentPresent(int index) {
+        return parentIndex(index) >= 0;
+    }
+
+    private void bubbleDown() {
+        int index = 1;
+
+        while(isLeftChildPresent(index)) {
+            if(leftChild(index).compareTo(rightChild(index)) < 0
+                && heap[index].compareTo(leftChild(index)) > 0) {
+                swap(index, leftChildIndex(index));
+                index = leftChildIndex(index);
+            } else if(rightChild(index).compareTo(leftChild(index)) < 0
+                    && heap[index].compareTo(rightChild(index)) > 0) {
+                swap(index, rightChildIndex(index));
+                index = rightChildIndex(index);
+            }
+        }
+    }
+
+    private boolean isLeftChildPresent(int index) {
+        return leftChildIndex(index) <= this.size;
+    }
+
+    private T parent(int index) {
+        return heap[parentIndex(index)];
+    }
+
+    private int parentIndex(int index) {
         return index / 2;
     }
 
-    private int leftChild(int index) {
+    private T leftChild(int index) {
+        return heap[leftChildIndex(index)];
+    }
+
+    private int leftChildIndex(int index) {
         return index * 2;
     }
 
-    private int rightChild(int index) {
+    private T rightChild(int index) {
+        return heap[rightChildIndex(index)];
+    }
+
+    private int rightChildIndex(int index) {
         return (index * 2) + 1;
     }
+
+
 
     private void swap(int first, int second) {
         T temp = heap[first];
