aabc4f1beacacbf12db9eca70eb5e54f4a590e81
jpassgo <jeffpascoe9@gmail.com>
2021-03-21 18:52:50 -0500

    Creating a method that will heapify an array as part of the heap sort process on an unordered array.

---
+++ src/main/java/Algorithms/Sorting/HeapSort.java
@@ -0,0 +1,33 @@
+package Algorithms.Sorting;
+
+public class HeapSort {
+
+    public static void main(String[] args) {
+        Integer[] array = new Integer[] {232, 5, 23, 2, 1, 443, 52};
+
+        for(int item : array) System.out.print(String.format("%s ", item));
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends Comparable> void heapify(T[] array, int index) {
+        int max = index;
+        int left = 2 * index;
+        int right = 2 * (index + 1);
+
+        if(left <= 0 && (array[left].compareTo(array[index]) > 0)) {
+            max = left;
+        }
+
+        if(right <= 0 && (array[right].compareTo(array[max]) > 0)) {
+            max = right;
+        }
+
+        if(max != index) {
+            T temp = array[max];
+            array[max] = array[index];
+            array[index] = temp;
+
+            heapify(array, max);
+        }
+    }
+}
